e88426d1021fcebfba7ea1969db6b8f930c7105d
jokercrab <anton20435@outlook.com>
2023-04-13 16:20:23 +0300

    updated details page

--- WuxiaApp/ViewModels/DetailsViewModel.cs
+++ WuxiaApp/ViewModels/DetailsViewModel.cs
@@ -1,29 +1,71 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using General.DataModels;
+using System.Diagnostics;
 using WuxiaApp.Servs;
+using WuxiaClassLib.DataModels;
+
 
 namespace WuxiaApp.ViewModels;
-
-public partial class DetailsViewModel : BaseViewModel
+//[QueryProperty(nameof(Name),"Book")]
+public partial class DetailsViewModel : BaseViewModel ,IQueryAttributable
 {
     Services services;
+
     [ObservableProperty]
     public Book book;
+
     public DetailsViewModel(Services services)
     {
-
         this.services = services;
-        book = new Book()
+        ImageParams = new Dictionary<string, string>
         {
-            Chapters = 120,
-            Description = "blah blah blah this is discription",
-            PicturePath = "https://wuxiaworldeu.b-cdn.net/original/versatile-mage.webp?width=400&quality=80",
-            Ratings = "5.0",
-            Readed = 120,
-            Title = "Some Book Is Here",
-            Views = "120k"
+            ["preview"] = ".webp",   //?width=150&quality=60",
+            ["source"] = "https://wuxiaworldeu.b-cdn.net/original/"
         };
-        this.Title = book.Title;
+    }
+
+    public async void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (IsBusy)
+            return;
+        
+        var slug = query["slug"] as String;
+        if (slug == null)
+            return;
+        await GetBookDataAsync(slug); 
+    }
+
+    async Task GetBookDataAsync(string name)
+    {
+        try
+        {
+
+            IsBusy = true;
+            var bookinfo = await services.GetBookInfoAsync(name);
+
+            var book = new Book()
+            {
+                Chapters = bookinfo.chapters,
+                Description = bookinfo.description,
+                Ratings = bookinfo.rating,
+                Title = bookinfo.name,
+                Views = bookinfo.views,
+                Slug = bookinfo.slug
+            };
+            if (bookinfo.image == null)
+                book.PicturePath = "unloaded_image.png";
+            else
+                book.PicturePath = ImageParams["source"] + bookinfo.slug + ImageParams["preview"];
+            Book = book;
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get books: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+
+        }
+        { IsBusy = false;}
+
     }
 }
 

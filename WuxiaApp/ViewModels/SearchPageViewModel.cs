63636105a125ac90b8ca3e0e19c659016e9a4ae7
jokercrab <anton20435@outlook.com>
2023-04-25 12:37:51 +0300

    implemented incremental loading

--- WuxiaApp/ViewModels/SearchPageViewModel.cs
+++ WuxiaApp/ViewModels/SearchPageViewModel.cs
@@ -15,8 +15,13 @@ namespace WuxiaApp.ViewModels;
 public partial class SearchPageViewModel : BaseViewModel
 {
     public ObservableCollection<Book> Books { get; } = new();
+    [ObservableProperty]
+    bool isLoadingNewData=false;
+    [ObservableProperty]
+    uint footer;
     
     readonly Services services;
+    Uri nextData;
     public SearchPageViewModel(Services services)
     {
         this.services = services;
@@ -37,7 +42,7 @@ public partial class SearchPageViewModel : BaseViewModel
         {
 
             IsBusy = true;
-
+            Footer = 100;
 #if ANDROID
 
         if (Platform.CurrentActivity.CurrentFocus != null)
@@ -46,7 +51,8 @@ public partial class SearchPageViewModel : BaseViewModel
             Platform.CurrentActivity.CurrentFocus.ClearFocus();
         }
 #endif
-            var searchresult = await services.SearchBookAsync(searchParam); 
+            var searchresult = await services.SearchBookAsync(searchParam);
+            nextData = new Uri(searchresult.next);
             if (Books.Count != 0)
                 Books.Clear();
             foreach (var result in searchresult.results)
@@ -93,6 +99,48 @@ public partial class SearchPageViewModel : BaseViewModel
         };
         await Shell.Current.GoToAsync(nameof(DetailsPage), query);
 
+    }
+    [RelayCommand]
+    async Task LoadNextDataChunkAsync()
+    {
+        if (IsBusy || nextData==null || IsLoadingNewData)
+        {
+            Footer = 0;
+            return;
+        }
+            
+        
+        try
+        {
+            IsLoadingNewData = true;
+            var searchResult = await services.LoadNextDataAsync(nextData);
+            nextData = searchResult.next == null ? null : new Uri(searchResult.next);
+            foreach (var result in searchResult.results)
+            {
+                var book = new Book()
+                {
+                    Chapters = result.chapters,
+                    Description = result.description,
+                    Ratings = result.rating,
+                    Title = result.name,
+                    Views = result.views,
+                    Slug = result.slug
+                };
+                if (result.image == null)
+                    book.PicturePath = "unloaded_image.png";
+                else
+                    book.PicturePath = services.FormPicturePath(result.slug);
+                Books.Add(book);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get books: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+
+        }finally { IsLoadingNewData = false; Footer = 60; }
+
+        
     }
 }
 

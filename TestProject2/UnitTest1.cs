1e2150789fa9f6f8bcc8a45927efa9066f6d700a
jokercrab <anton20435@outlook.com>
2023-05-20 12:21:26 +0300

    added new tests

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -1,14 +1,16 @@
 using WuxiaApp.Servs;
 using Xunit.Abstractions;
 using WuxiaClassLib.DataModels;
+using Scraper;
+using General.DataModels;
 
 namespace TestProject2
 {
-    public class UnitTest1
+    public class ServicesTests
     {
         private readonly ITestOutputHelper output;
 
-        public UnitTest1(ITestOutputHelper output)
+        public ServicesTests(ITestOutputHelper output)
         {
             this.output = output;
         }
@@ -60,6 +62,37 @@ namespace TestProject2
         {
            var service = new Services();
            var output = await service.LoadNextDataAsync(new Uri("http://wuxia.click/api/search/?limit=10&offset=10&search=mar"));
+        }
+    }
+    public class ScraperTests
+    {
+        [Fact]
+        public void ReloadThrowsExceptionWhenNoLinkProvided()
+        {
+            var scraper = new WuxiaScraper();
+            Assert.Throws<NullReferenceException>(()=>scraper.Reload());
+        }
+        [Fact]
+        public void LoadChangesStoredPageLink()
+        {
+            var scraper = new WuxiaScraper("https://wuxia.click/chapter/versatile-mage-2");
+            var old = scraper.SiteUri;
+            scraper.Load("https://wuxia.click/chapter/versatile-mage-3");
+            Assert.Equal(old, scraper.SiteUri);
+
+        }
+        [Fact]
+        public async void GetReadingPageReturnsProperData()
+        {
+            var scraper = new WuxiaScraper();
+            scraper.Load("https://wuxia.click/chapter/lord-of-the-mysteries-1");
+           
+            var result = scraper.GetScriptContentDOMAsync<ChapterData>();
+            
+        }
+        [Fact]
+        public void WhyInTheFuckItsNotWorking()
+        {
         }
     }
 }

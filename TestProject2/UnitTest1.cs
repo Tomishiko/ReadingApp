0c960c75e52cde7547dbc3b7b8c6a4e02f78c401
jokercrab <anton20435@outlook.com>
2023-07-11 15:41:29 +0300

    update tests

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -3,7 +3,7 @@ namespace TestProject2
     public class ServicesTests
     {
         private readonly ITestOutputHelper output;
-        
+
         public ServicesTests(ITestOutputHelper output)
         {
             this.output = output;
@@ -12,14 +12,14 @@ namespace TestProject2
         public async void SearchBookThrowsExceptionWhenHttpUnsuccessful()
         {
             var service = new BaseServices();
-            var result = await service.SearchBookAsync();
+            //var result = await service.SearchBookAsync();
 
         }
         [Fact]
         public async void NewHttpRequestTest()
         {
             var servs = new BaseServices();
-            var output = await servs.SearchBookAsync("martial");   
+            var output = await servs.SearchBookAsync("martial");
         }
 
         [Fact]
@@ -47,51 +47,52 @@ namespace TestProject2
         public void FormPicPathThrowsExceptionWhenNameNull()
         {
             var service = new BaseServices();
-            Assert.Throws<ArgumentNullException>(() =>service.FormPicturePath(null));
+            Assert.Throws<ArgumentNullException>(() => service.FormPicturePath(null));
         }
         [Fact]
         public void FormPicPathThrowsExceptionWhenParamNull()
         {
             var service = new BaseServices();
-            Assert.Throws<ArgumentNullException>(() => 
+            Assert.Throws<ArgumentNullException>(() =>
             service.FormPicturePath("blah")
                 );
         }
         [Fact]
         public async void NextDataMethodReturnsProperResult()
         {
-           var service = new BaseServices();
-           var output = await service.LoadNextDataAsync(new Uri("http://wuxia.click/api/search/?limit=10&offset=10&search=mar"));
+            var service = new BaseServices();
+            var output = await service.LoadNextDataAsync(new Uri("http://wuxia.click/api/search/?limit=10&offset=10&search=mar"));
         }
         [Fact]
         public async Task SaveLoadFunctionality()
         {
         }
-    public class ScraperTests
-    {
-        [Fact]
-        public void ReloadThrowsExceptionWhenNoLinkProvided()
+        public class ScraperTests
         {
-            var scraper = new WuxiaScraper();
-            //Assert.Throws<NullReferenceException>(()=>scraper.Reload());
-        }
-        [Fact]
-        public void LoadChangesStoredPageLink()
-        {
-            var scraper = new WuxiaScraper("https://wuxia.click/chapter/versatile-mage-2");
-            var old = scraper.SiteUri;
-            //scraper.Load("https://wuxia.click/chapter/versatile-mage-3");
-            Assert.Equal(old, scraper.SiteUri);
+            [Fact]
+            public void ReloadThrowsExceptionWhenNoLinkProvided()
+            {
+                var scraper = new WuxiaScraper();
+                //Assert.Throws<NullReferenceException>(()=>scraper.Reload());
+            }
+            [Fact]
+            public void LoadChangesStoredPageLink()
+            {
+                var scraper = new WuxiaScraper("https://wuxia.click/chapter/versatile-mage-2");
+                var old = scraper.SiteUri;
+                //scraper.Load("https://wuxia.click/chapter/versatile-mage-3");
+                Assert.Equal(old, scraper.SiteUri);
 
-        }
-        [Fact]
-        public async void GetReadingPageReturnsProperData()
-        {
-            var scraper = new WuxiaScraper();
-            //scraper.Load("https://wuxia.click/chapter/lord-of-the-mysteries-1");
-           
-            var result = scraper.GetScriptContentDOMAsync<ChapterData>();
-            
+            }
+            [Fact]
+            public async void GetReadingPageReturnsProperData()
+            {
+                var scraper = new WuxiaScraper();
+                //scraper.Load("https://wuxia.click/chapter/lord-of-the-mysteries-1");
+
+                var result = scraper.GetScriptContentDOMAsync<ChapterData>();
+
+            }
         }
     }
     public class ApiTests
@@ -105,13 +106,31 @@ namespace TestProject2
             var expected = JsonSerializer.Deserialize<searchResult>(File.ReadAllText("SearchResultStandart.json"));
             Assert.Equal(expected, result);
         }
+        [Fact]
         public async void SearchBookThrowsExceptionWhenFails()
         {
-            var responseMsg = new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
-            var httpclientMock = new Mock<HttpClient>();
-
-            service.SearchBookAsync();
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.NotFound);
+            var httpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            httpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(response);
+            BaseServicesMoq mocked = new BaseServicesMoq(httpMessageHandlerMock);
+            await Assert.ThrowsAsync<HttpRequestException>(() => service.SearchBookAsync());
         }
 
     }
+
+    class BaseServicesMoq : BaseServices
+    {
+        public BaseServicesMoq(Mock<HttpMessageHandler> mock)
+        {
+            client = new HttpClient(mock.Object);
+            client.BaseAddress = new Uri("https://www.wuxia/api");
+        }
+    }
+
 }
--- TestProject2/Usings.cs
+++ TestProject2/Usings.cs
@@ -5,5 +5,6 @@ global using System.Text.Json;
 global using Scraper;
 global using General.DataModels;
 global using Moq;
+global using Moq.Protected;
 global using Microsoft.Maui;
 global using WuxiaClassLib.DataModels;
